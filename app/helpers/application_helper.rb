24ec49d223006925f77c37b8be571579670be3e7
ilyakrivonozhko <ilya.krivonozhko@gmail.com>
2023-06-22 16:08:36 +0800

    A helper has been added to indicate the selected item in the main menu.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,19 @@ module ApplicationHelper
       base_title
     end
   end
+
+  def currently_at(current_page = "")
+    render partial: 'shared/menu', locals: {current_page: current_page}
+  end
+
+  def nav_tab(title, url, options = {})
+    current_page = options.delete :current_page
+    css_class = current_page == title ? 'text-secondary' : 'text-white'
+    options[:class] = if options[:class]
+                        options[:class] + ' ' + css_class
+                      else
+                        css_class
+                      end
+    link_to title, url, options
+  end
 end
