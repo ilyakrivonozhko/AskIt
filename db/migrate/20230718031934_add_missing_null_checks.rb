e13cb5e399e7edbad911fce8342e5393f740fd84
ilyakrivonozhko <ilya.krivonozhko@gmail.com>
2023-07-18 11:23:06 +0800

    Added validation at the database level: the question must have a body and a title.

---
+++ db/migrate/20230718031934_add_missing_null_checks.rb
@@ -0,0 +1,6 @@
+class AddMissingNullChecks < ActiveRecord::Migration[7.0]
+  def change
+    change_column_null :questions, :title, false
+    change_column_null :questions, :body, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_17_123646) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_18_031934) do
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
     t.integer "question_id", null: false
@@ -20,8 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_17_123646) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
+    t.string "title", null: false
+    t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
