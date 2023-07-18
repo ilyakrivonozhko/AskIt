d9f448f7cd9433d0e558b25d7877df9ba296dcee
ilyakrivonozhko <ilya.krivonozhko@gmail.com>
2023-07-18 12:01:03 +0800

    Moved pagination to helper, fixed display of pagination buttons when there are no items.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 module ApplicationHelper
   include Pagy::Frontend
 
+  def pagination(obj)
+    raw (pagy_bootstrap_nav(obj)) if obj.pages > 1
+  end
+
   def full_title(page_title = "")
     base_title = "AskIt"
     if page_title.present?
