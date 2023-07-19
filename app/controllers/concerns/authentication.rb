dba3619adffa141cee1bd4066eaf2b7628358b32
ilyakrivonozhko <ilya.krivonozhko@gmail.com>
2023-07-19 14:09:37 +0800

    Added remembering the user and storing data about it in cookies.

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -4,8 +4,25 @@ module Authentication
     private
     
     def current_user
-      @current_user ||= User.find_by(id: session[:user_id]).decorate if session[:user_id].present?
+      user = session[:user_id].present? ? user_from_session : user_from_token
+
+      @current_user ||= user&.decorate
     end
+
+    def user_from_session
+      User.find_by(id: session[:user_id])
+    end
+
+    def user_from_token
+      user = User.find_by(id: cookies.encrypted[:user_id])
+      token = cookies.encrypted[:remember_token]
+
+      return unless user&.remember_token_authenticated?(token)
+
+      sign_in user
+      user
+    end
+
 
     def user_signed_in?
       current_user.present?
@@ -16,6 +33,7 @@ module Authentication
     end
 
     def sign_out
+      forget current_user
       session.delete :user_id
       @current_user = nil
     end
@@ -30,6 +48,18 @@ module Authentication
       return unless user_signed_in?
       flash[:warning] = "You are already signed in!"
       redirect_to root_path
+    end
+
+    def remember(user)
+      user.remember_me
+      cookies.encrypted.permanent[:remember_token] = user.remember_token
+      cookies.encrypted.permanent[:user_id] = user.id
+    end
+
+    def forget(user)
+      user.forget_me
+      cookies.delete :user_id
+      cookies.delete :remember_token
     end
     
     helper_method :current_user, :user_signed_in?
